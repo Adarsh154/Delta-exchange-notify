9393fd6261df50239fcc7ba1b8c99c85a7120384
AJ <aj1541998@gmail.com>
2022-04-04 21:29:25 +0530

    changed script to put buy and sell

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import concurrent.futures
+import time
+from copy import deepcopy
+
 import requests
 from datetime import datetime, timedelta
 import telegram
@@ -10,27 +13,32 @@ logging.basicConfig(filename="std.log",
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+count = 0
+local_count = 0
+
 
 def get_prices(strike):
+    global count, local_count
+    mode = "sell" if local_count < count else "buy"
     try:
         headers = {
             'Accept': 'application/json'
         }
-        if strike[0] == "C":
+        local_count += 1
+        if mode == "sell":
             return strike, requests.get('https://api.delta.exchange/v2/l2orderbook/{}'.format(strike), params={
-            }, headers=headers).json()['result']['buy'][0]['price']
+            }, headers=headers).json()['result'][mode][0]['price']
         else:
             return strike, requests.get('https://api.delta.exchange/v2/l2orderbook/{}'.format(strike), params={
-            }, headers=headers).json()['result']['sell'][0]['price']
+            }, headers=headers).json()['result'][mode][0]['price']
     except Exception as get_price:
-        logger.error(get_price)
+        logger.error("get_price Error:" + str(get_price))
 
 
 # Api call to get strike price symbols
 def get_strike_prices(coin):
-    all_calls = list()
-    all_puts = list()
-
+    all_puts_sell = list()
+    global count
     try:
         headers = {
             'Accept': 'application/json'
@@ -39,53 +47,52 @@ def get_strike_prices(coin):
         }, headers=headers).json()['result']
 
         for i in r:
-            if i['description'] in ['{} put option expiring on {}'.format(coin, date_refined),
-                                    '{} call option expiring on {}'.format(coin, date_refined)]:
-                if i['symbol'][0] == 'C':
-                    all_calls.append(i['symbol'])
-                else:
-                    all_puts.append(i['symbol'])
+            if i['description'] == '{} put option expiring on {}'.format(coin, date_refined):
+                all_puts_sell.append(i['symbol'])
 
     except Exception as symbol_error:
-        logger.error(symbol_error)
+        logger.error("symbol_error Error" + str(symbol_error))
 
+    all_puts_buy = deepcopy(all_puts_sell)
+    count = len(all_puts_buy)
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        all_calls = list(executor.map(get_prices, all_calls))
-        all_puts = list(executor.map(get_prices, all_puts))
+        all_puts_sell = list(executor.map(get_prices, all_puts_sell))
+        all_puts_buy = list(executor.map(get_prices, all_puts_buy))
 
-    all_calls.sort(key=lambda x: x[0])
-    all_puts.sort(key=lambda x: x[0])
-    return all_calls, all_puts
+    all_puts_buy.sort(key=lambda x: x[0])
+    all_puts_sell.sort(key=lambda x: x[0])
+    return all_puts_buy, all_puts_sell
 
 
 if __name__ == "__main__":
     current_hour = datetime.now().hour
     message_sent = {current_hour: []}
     while True:
+        time.sleep(3)
         # Get today's or tomorrow's date based on time
         day = (datetime.now()).strftime('%d-%m-%Y')
-        if datetime.now().hour > 17 or (datetime.now().hour == 17 and datetime.now().minute > 30):
+        if datetime.now().hour > 17 or (datetime.now().hour == 17 and datetime.now().minute >= 30):
             day = (datetime.now() + timedelta(1)).strftime('%d-%m-%Y')
         date_refined = '' if day[0] == '0' else day[0]
         date_refined = date_refined + day[1:3]
         date_refined = date_refined + '' if day[3] == '0' else day[3]
         date_refined = date_refined + day[4:]
-        calls, puts = get_strike_prices(coin="ETH")
+        puts_buy, puts_sell = get_strike_prices(coin="ETH")
 
         c, p = 0, 1
-        while c < len(calls) - 1:
-            if p == len(calls) - 1:
+        while c < len(puts_buy) - 1:
+            if p == len(puts_buy) - 1:
                 c += 1
                 p = c + 1
             else:
-                if calls[c][1] > puts[p][1]:
-                    to_send = str(calls[c] + puts[p])
+                if float(puts_buy[c][1]) > float(puts_sell[p][1]):
+                    to_send = str(puts_buy[c] + puts_sell[p])
                     if datetime.now().hour == current_hour:
                         if to_send not in message_sent[current_hour]:
                             status, error_message = telegram.send_message(to_send)
                             message_sent[current_hour].append(to_send)
                             if not status:
-                                logger.error(error_message)
+                                logger.error("Message send error" + error_message)
                     else:
                         current_hour = datetime.now().hour
                         message_sent = {datetime.now().hour: []}
@@ -93,5 +100,5 @@ if __name__ == "__main__":
                             status, error_message = telegram.send_message(to_send)
                             message_sent[current_hour].append(to_send)
                             if not status:
-                                logger.error(error_message)
+                                logger.error("Message send error" + error_message)
                 p += 1
