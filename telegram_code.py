78e72569e8a64985591e12960f6edc702e091fda
AJ <aj1541998@gmail.com>
2022-04-26 23:31:33 +0530

    cp and change pp

--- telegram_code.py
+++ telegram_code.py
@@ -13,17 +13,28 @@ updater = Updater(token,
                   use_context=True)
 sticker_id = "CAACAgUAAxkBAAETZyJiaC5D_9ue30Ae6sHQ1SogU5s7fgACGQEAAqP7yVR9wpCHJCufxiQE"
 
+
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hello sir, Welcome to the Bot.Please write help to see the commands available.")
 
 
 def get_processes(update: Update, context: CallbackContext):
     running_process = os.popen("ps -aef | grep -i 'python3' | grep -v 'grep'").read().strip().split('\n')
+    if not len(running_process) == 5:
+        update.message.reply_text("Something not right, check processes---> Contact AJ")
+        for process in running_process:
+            update.message.reply_text(process)
+    else:
+        update.message.reply_text("ALL GOOD")
+        url = "https://api.telegram.org/bot{}/sendSticker?chat_id={}&sticker={}".format(
+            token, update.message.chat.id, sticker_id)
+        _ = requests.get(url)
+
+
+def check_processes(update: Update, context: CallbackContext):
+    running_process = os.popen("ps -aef | grep -i 'python3' | grep -v 'grep'").read().strip().split('\n')
     for process in running_process:
         update.message.reply_text(process)
-    url = "https://api.telegram.org/bot{}/sendSticker?chat_id={}&sticker={}".format(
-        token, update.message.chat.id, sticker_id)
-    _ = requests.get(url)
 
 
 def get_logs(update: Update, context: CallbackContext):
@@ -45,6 +56,7 @@ def unknown_text(update: Update, context: CallbackContext):
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('pp', get_processes))
+updater.dispatcher.add_handler(CommandHandler('cp', check_processes))
 updater.dispatcher.add_handler(CommandHandler('logs', get_logs))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(
