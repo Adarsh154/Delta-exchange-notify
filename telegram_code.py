e114109eca1aa170e553bf61ad673026ca0047c0
AJ <aj1541998@gmail.com>
2022-04-26 23:25:08 +0530

    Add sticker

--- telegram_code.py
+++ telegram_code.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import requests
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -7,11 +8,10 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 
-
 token = os.getenv("ttoken")
 updater = Updater(token,
                   use_context=True)
-
+sticker_id = "CAACAgUAAxkBAAETZyJiaC5D_9ue30Ae6sHQ1SogU5s7fgACGQEAAqP7yVR9wpCHJCufxiQE"
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hello sir, Welcome to the Bot.Please write help to see the commands available.")
@@ -21,6 +21,9 @@ def get_processes(update: Update, context: CallbackContext):
     running_process = os.popen("ps -aef | grep -i 'python3' | grep -v 'grep'").read().strip().split('\n')
     for process in running_process:
         update.message.reply_text(process)
+    url = "https://api.telegram.org/bot{}/sendSticker?chat_id={}&sticker={}".format(
+        token, update.message.chat.id, sticker_id)
+    _ = requests.get(url)
 
 
 def get_logs(update: Update, context: CallbackContext):
@@ -51,4 +54,3 @@ updater.dispatcher.add_handler(MessageHandler(
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
 
 updater.start_polling()
-
