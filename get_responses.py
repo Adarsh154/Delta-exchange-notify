68d60dbc0774cfc9acf330e9aada8eefb23f81c6
AJ <aj1541998@gmail.com>
2022-09-10 00:19:55 +0530

    v0.1.1 dev

---
+++ get_responses.py
@@ -0,0 +1,137 @@
+import requests
+
+url = "https://p-api.delta.exchange/v2/tickers?contract_types=call_options,put_options"
+
+payload = {}
+headers = {}
+
+
+# def get_dates():
+#     date_refined = [""] * 2
+#     initial_day = datetime.now(tz=gettz('Asia/Kolkata'))
+#     i, j = 0, 3
+#     if (initial_day.hour > 17) or (initial_day.hour == 17 and initial_day.minute >= 30):
+#         if initial_day.weekday() ==4:
+#             i=1
+#             j=4
+#         initial_day = initial_day + timedelta(1)
+#     date_refined[0] = initial_day.strftime('%d-%m-%Y')
+#     date_refined[1] = (initial_day + timedelta(1)).strftime('%d-%m-%Y')
+#     friday = initial_day + timedelta(4 - initial_day.weekday())
+#     while True:
+#         if i == j:
+#             break
+#         next_friday = friday + i * timedelta(7)
+#         if next_friday.strftime('%d-%m-%Y') not in date_refined:
+#             date_refined.append(next_friday.strftime('%d-%m-%Y'))
+#             i += 1
+#
+#     for i in range(2, 4):
+#         last_day = (date(initial_day.year, initial_day.month + i, 1) - timedelta(days=1))
+#         num = last_day.weekday() % 4
+#         if last_day.weekday() < 4:
+#             num = 3 + last_day.weekday()
+#         x = (last_day - timedelta(num)).strftime('%d-%m-%Y')
+#         if x not in date_refined:
+#             date_refined.append(x)
+#
+#     for d in range(len(date_refined)):
+#         day = date_refined[d]
+#         new_date = '' if day[0] == '0' else day[0]
+#         new_date = new_date + day[1:3]
+#         #new_date = new_date + ('' if day[3] == '0' else day[3])
+#         new_date = new_date + day[3:5]
+#         new_date = new_date + day[8:]
+#         date_refined[d] = new_date.replace("-", "")
+#         # date_refined[d] = new_date
+#
+#     return date_refined
+
+
+def get_strike_price_from_symbol(text, coin):
+    new_text = text[text.find(coin) + 4:]
+    return float(new_text[:new_text.find("-")])
+
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start + len(needle))
+        n -= 1
+    return start
+
+
+def get_master_response():
+    response = requests.get(url, headers=headers, data=payload)
+    BTC_call_buy_master = dict()
+    BTC_put_buy_master = dict()
+    BTC_call_sell_master = dict()
+    BTC_put_sell_master = dict()
+
+    ETH_call_buy_master = dict()
+    ETH_put_buy_master = dict()
+    ETH_call_sell_master = dict()
+    ETH_put_sell_master = dict()
+
+    for r in response.json()['result']:
+        date = r['symbol'][find_nth(r['symbol'], "-", 3) + 1:]
+        if 'BTC' in r['symbol']:
+            if r['contract_type'] == "call_options":
+                if date not in BTC_call_buy_master:
+                    BTC_call_buy_master[date] = list()
+                    BTC_call_sell_master[date] = list()
+                BTC_call_buy_master[date].append(
+                    (r['symbol'], r['quotes']['best_bid'], 'size={}'.format(r['quotes']['bid_size'])))
+                BTC_call_sell_master[date].append(
+                    (r['symbol'], r['quotes']['best_ask'], 'size={}'.format(r['quotes']['ask_size'])))
+            else:
+                if date not in BTC_put_buy_master:
+                    BTC_put_buy_master[date] = list()
+                    BTC_put_sell_master[date] = list()
+                BTC_put_buy_master[date].append(
+                    (r['symbol'], r['quotes']['best_bid'], 'size={}'.format(r['quotes']['bid_size'])))
+                BTC_put_sell_master[date].append(
+                    (r['symbol'], r['quotes']['best_ask'], 'size={}'.format(r['quotes']['ask_size'])))
+        elif 'ETH' in r['symbol']:
+            if r['contract_type'] == "call_options":
+                date = r['symbol'][find_nth(r['symbol'], "-", 3) + 1:]
+                if date not in ETH_call_buy_master:
+                    ETH_call_buy_master[date] = list()
+                    ETH_call_sell_master[date] = list()
+                ETH_call_buy_master[date].append(
+                    (r['symbol'], r['quotes']['best_bid'], 'size={}'.format(r['quotes']['bid_size'])))
+                ETH_call_sell_master[date].append(
+                    (r['symbol'], r['quotes']['best_ask'], 'size={}'.format(r['quotes']['ask_size'])))
+            else:
+                if date not in ETH_put_buy_master:
+                    ETH_put_buy_master[date] = list()
+                    ETH_put_sell_master[date] = list()
+                ETH_put_buy_master[date].append(
+                    (r['symbol'], r['quotes']['best_bid'], 'size={}'.format(r['quotes']['bid_size'])))
+                ETH_put_sell_master[date].append(
+                    (r['symbol'], r['quotes']['best_ask'], 'size={}'.format(r['quotes']['ask_size'])))
+
+    # call_buy.sort(key=lambda x: my_func(x[0], 'BTC'), reverse=True)
+    # call_sell.sort(key=lambda x: my_func(x[0], 'BTC'), reverse=True)
+    for i in BTC_call_sell_master.keys():
+        BTC_call_buy_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "BTC"), reverse=True)
+        BTC_call_sell_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "BTC"), reverse=True)
+        BTC_put_buy_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "BTC"))
+        BTC_put_sell_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "BTC"))
+
+        ETH_call_buy_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "ETH"), reverse=True)
+        ETH_call_sell_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "ETH"), reverse=True)
+        ETH_put_buy_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "ETH"))
+        ETH_put_sell_master[i].sort(key=lambda x: get_strike_price_from_symbol(x[0], "ETH"))
+
+    btc_call_club = []
+    btc_puts_club = []
+    eth_call_club = []
+    eth_puts_club = []
+    for i in BTC_call_sell_master.keys():
+        btc_call_club.append([BTC_call_buy_master[i], BTC_call_sell_master[i], i])
+        btc_puts_club.append([BTC_put_buy_master[i], BTC_put_sell_master[i], i])
+
+        eth_call_club.append([ETH_call_buy_master[i], ETH_call_sell_master[i], i])
+        eth_puts_club.append([ETH_put_buy_master[i], ETH_put_sell_master[i], i])
+    return btc_call_club, btc_puts_club, eth_call_club, eth_puts_club
--- main_eth_put.py
+++ main_eth_put.py
@@ -1,54 +1,13 @@
+import concurrent.futures
 import time
-from dateutil.tz import gettz
-from datetime import datetime, timedelta
-import utilities
-from utilities import logger
+
+import ETH
+import get_responses
 
 if __name__ == "__main__":
-    current_hour = datetime.now(tz=gettz('Asia/Kolkata')).hour
-    message_sent = {current_hour: []}
-    while True:
-        time.sleep(45)
-        # Get today's or tomorrow's date based on time
-        day = (datetime.now(tz=gettz('Asia/Kolkata'))).strftime('%d-%m-%Y')
-        if datetime.now(tz=gettz('Asia/Kolkata')).hour > 15:
-            day = (datetime.now(tz=gettz('Asia/Kolkata')) + timedelta(1)).strftime('%d-%m-%Y')
-        date_refined = '' if day[0] == '0' else day[0]
-        date_refined = date_refined + day[1:3]
-        date_refined = date_refined + '' if day[3] == '0' else day[3]
-        date_refined = date_refined + day[4:]
-        try:
-            puts_buy, puts_sell = utilities.get_strike_prices("ETH", date_refined, "put")
-        except Exception as e:
-            logger.error(str(e))
-            # utilities.send_message(str(e))
-            continue
 
-        c, p = 0, 1
-        while c < len(puts_buy) - 1:
-            if p == len(puts_buy) - 1:
-                c += 1
-                p = c + 1
-            else:
-                diff_plain = float(int((float(puts_buy[c][1]) - float(puts_sell[p][1])) * 100) / 100)
-                if diff_plain >= 0.00:
-                    diff_with_charges = diff_plain - ((float(puts_buy[c][1]) + float(puts_sell[p][1])) * 0.1)
-                    to_send = "Sell-" + str(puts_buy[c]) + ", Buy-" + str(puts_sell[p]) + \
-                              " \n Diff_plain = {}\nDiff_with_charges = {}".format(
-                                  diff_plain, diff_with_charges)
-                    if datetime.now(tz=gettz('Asia/Kolkata')).hour == current_hour:
-                        if to_send not in message_sent[current_hour]:
-                            status, error_message = utilities.send_message(to_send, False)
-                            message_sent[current_hour].append(to_send)
-                            if not status:
-                                logger.error("Message send error" + error_message)
-                    else:
-                        del message_sent[current_hour]
-                        current_hour = datetime.now(tz=gettz('Asia/Kolkata')).hour
-                        message_sent = {current_hour: []}
-                        if to_send not in message_sent[current_hour]:
-                            status, error_message = utilities.send_message(to_send, False)
-                            message_sent[current_hour].append(to_send)
-                            if not status:
-                                logger.error("Message send error" + error_message)
-                p += 1
+    while True:
+        time.sleep(10)
+        btc_call_club, btc_puts_club, eth_call_club, eth_puts_club = get_responses.get_master_response()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(ETH.ev_eth, eth_puts_club)
